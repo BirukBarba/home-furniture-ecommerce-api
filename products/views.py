b3a24693ec22744cea155fa0fb8f53528ff31897
birukbarba <birukbarba@gmail.com>
2025-12-24 21:36:19 +0300

    filtering by category added

--- products/views.py
+++ products/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from rest_framework import viewsets, filters
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -14,5 +15,11 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [
+        filters.SearchFilter,
+        DjangoFilterBackend
+    ]
+
     search_fields = ['name', 'category__name']
+    filterset_fields = ['category']
+
